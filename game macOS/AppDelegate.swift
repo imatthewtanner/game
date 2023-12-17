77d3c780067c2d3c03c83527ae77883c339985d4
imatthewtanner <imatthewtanner@icloud.com>
2023-12-16 19:58:02 -0600

    Initial Commit

---
+++ game macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  game macOS
+//
+//  Created by Matthew Tanner on 12/16/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
